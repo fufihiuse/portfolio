9081f9a2d9573e8a9d5f838f5c2bc47ca03d201f
Jackson Majewski <jacksonmajewski@gmail.com>
2024-12-06 14:37:29 -0500

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './client/*.{html,js,handlebars}',
-    './server/views/*.{html,js,handlebars}',
+    './client/*.{html,js,handlebars,jsx}',
+    './server/views/*.{html,js,handlebars,jsx}',
   ],
   theme: {
     extend: {},
