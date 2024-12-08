749e3bb16ace6148a0dcaf23cda0c61b2e57ef9b
Jackson Majewski <jacksonmajewski@gmail.com>
2024-12-08 17:16:39 -0500

    fixed sorting

--- server/models/PortfolioPiece.js
+++ server/models/PortfolioPiece.js
@@ -1,29 +1,34 @@
 const mongoose = require('mongoose');
 
 const PortfolioPieceSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  role: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  imgPath: {
-    type: String,
-    required: true,
-  },
-  link: {
-    type: String,
-    required: true,
-  },
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    role: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    imgPath: {
+        type: String,
+        required: true,
+    },
+    link: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        required: true,
+    }
 });
 
 const PortfolioModel = mongoose.model('PortfolioPiece', PortfolioPieceSchema);
